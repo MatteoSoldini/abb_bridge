89e575d554cf3adba62bcc5c629d84f723a4c2f3
Cristian Livella <cristian@cristianlivella.com>
2020-02-03 18:35:36 +0100

    Update abb_bridge.py
    Gestione errori, rimossa password hard-coded

--- abb_bridge.py
+++ abb_bridge.py
@@ -1,14 +1,28 @@
 import time
 import requests
+requests.packages.urllib3.disable_warnings()
 
-password = 'mnnkR8TewKdsf9nEwRBFkNvzAQJceTkBWrgchXVjjna7u2jKejKLfKmnx2Cgsc5C'
+password = 'INSERT_PASSWORD_HERE'
 
+print("Daemon started")
 while True:
-    r = requests.get('https://abb.cristianlivella.com/get.php', auth=('abb', password), verify=False)
-    if r.status_code is 200:
-        if r.text is not '0':
-            requests.get('http://192.168.4.1/pos' + r.text)
-            print(r.text)
-    else:
-        print('Error server')
+    try:
+        try:
+            r = requests.get('https://abb.cristianlivella.com/get.php', auth=('abb', password), verify=False, timeout=5)
+        except:
+            print("No response from server")
+            continue
+        if r.status_code is 200:
+            if r.text is not '0':
+                print('Server say: ' + r.text)
+                try:
+                    requests.get('http://192.168.4.1/pos' + r.text, timeout=5)
+                except:
+                    print('No response from ESP')
+            else:
+                print('Server say nothing')
+        else:
+            print('Server error')
+    except:
+	print("Fatal error!")
     time.sleep(0.2)
